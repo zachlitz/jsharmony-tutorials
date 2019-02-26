f0ff3d96b70061f7fd73803d31d6e0e488b386bf
dkomsa <dkomsa@zacks.com>
2019-02-26 12:02:24 -0600

    moved run app to same process. run compare images async
    Signed-off-by: dkomsa <dkomsa@zacks.com>

--- test/compare-screenshots.js
+++ test/compare-screenshots.js
@@ -1,5 +1,6 @@
 var gm = require('gm');
 var imageMagick = gm.subClass({ imageMagick: true });
+var im = imageMagick();
 var should = require('should');
 const fs = require("fs");
 const path = require("path");
@@ -10,7 +11,7 @@ const test_dir = path.dirname(__filename);
 const data_dir = path.join(test_dir,'test-app','data');
 const result_file = path.join(test_dir,'comparing-screenshots-test.html');
 let live_screenshots_path = path.join(test_dir,'..','public','screenshots');
-let regenerated_screenshots_path = path.join(test_dir,'screenshots');
+let test_screenshots_path = path.join(test_dir,'screenshots');
 let diff_screenshots_path = path.join(test_dir,'diff_screenshots');
 let global_test_state = 'passed';
 
@@ -24,52 +25,96 @@ before(function(done) {
   this.timeout(600000); // set test timeout long process to generate screenshots
   console.log('\n\rBefore Test global');
   HelperFS.createFolderIfNotExistsSync(data_dir);
-  HelperFS.rmdirRecursiveSync(regenerated_screenshots_path);
-  HelperFS.createFolderIfNotExistsSync(regenerated_screenshots_path);
+  HelperFS.rmdirRecursiveSync(test_screenshots_path);
+  HelperFS.createFolderIfNotExistsSync(test_screenshots_path);
   HelperFS.rmdirRecursiveSync(diff_screenshots_path);
   HelperFS.createFolderIfNotExistsSync(diff_screenshots_path);
-  var cp = require('child_process');
-  var n = cp.fork(test_dir + '/test-app/app.test.js');
-  n.addListener('exit',function (err) {
-    var m= 'Finished Image Generation';
-    if (err){
-      m+= ' with Error: '+ err.toString();
-      done(new Error(m));
-    }else{
-      console.log(m);
-      done();
-    }
-  });
+  start_harmony(done);
 });
 
 after(function () {
-  console.log('after');
+  console.log('after Global tests. State: '+ global_test_state);
   if (global_test_state === 'passed') {
     HelperFS.rmdirRecursiveSync(data_dir);
-    HelperFS.rmdirRecursiveSync(regenerated_screenshots_path);
+    HelperFS.rmdirRecursiveSync(test_screenshots_path);
     HelperFS.rmdirRecursiveSync(diff_screenshots_path);
     HelperFS.rmdirRecursiveSync(path.join(test_dir,'test-app','models'));
   }
 });
 
 
+
+
 describe('Compare Screenshots', function() {
   it('should directory: public/screenshots exist and have files', async function() {
-    await checkDirectory(live_screenshots_path)
+    checkDirectory(live_screenshots_path)
   });
 
-  it('should existing and generated images be equal', async function() {
+  it('should directory: test/screenshots exist and have files', async function() {
+    checkDirectory(test_screenshots_path)
+  });
+
+  // waits for don to be called (compares 5 images at the time)
+  it('should existing and generated images be equal DONE', function(done) {
+    this.timeout(60000);
+    let files = fs.readdirSync(live_screenshots_path);
+    console.log('# of existing images to test '+files.length);
+    console.log('# of generated images to test '+fs.readdirSync(test_screenshots_path).length);
+    let failImages = [];
+    async.eachLimit(
+      files,
+      5,
+      function(imageName,callback){
+        compareScreenshots(imageName,0.01).then(function (isEqual) {
+          // console.log('isEqual: '+isEqual);
+          if (!isEqual){
+            failImages.push({name:imageName,reason: 'Images not the same.'});
+          }
+          callback(null,failImages);
+        }).catch(function (err) {
+          failImages.push({name:imageName,reason:'New image not exist'});
+          callback(null,failImages);
+        });
+      }
+      ,function (er) {
+        if(er){
+          done(new Error('Comparison fails. Error:'+er));
+        }else{
+          try {
+            failImages.length.should.lessThan(1);
+            done();
+          }catch (e) {
+            console.log('# of fail images(not generated and not the same): '+failImages.length);
+            generateFailImagesResultPage(failImages).then(
+              function (html) {
+                fs.writeFile(result_file, html,{}, function(err, data){
+                  if (err) console.log(err);
+                  console.log("Result successfully written to File.");
+                  done(new Error('Some fails. Please check : '+result_file));
+                });
+              }
+            ).catch(function (er) {
+              done(new Error('Some fails, but can\'t generate errors file. Error:'+er));
+            });
+          }
+        }
+      }
+    )
+  });
+
+  // waits for return of promise (compares elem in sync way)
+  it('should existing and generated images be equal ASYNC', async function() {
     this.timeout(600000);
     let isImagesEqual=false;
     let files = fs.readdirSync(live_screenshots_path);
     console.log('# of existing images to test '+files.length);
-    console.log('# of generated images to test '+fs.readdirSync(regenerated_screenshots_path).length);
+    console.log('# of generated images to test '+fs.readdirSync(test_screenshots_path).length);
     let imageName='';
     let failImages = [];
     for( let i=0; i<files.length; i++){
       isImagesEqual=false;
       imageName = files[i];
-      if(!fs.existsSync(path.join(regenerated_screenshots_path,imageName))){
+      if(!fs.existsSync(path.join(test_screenshots_path,imageName))){
         failImages[imageName]={name:imageName,reason:'New image not exist'};
         continue;
       }
@@ -88,15 +133,15 @@ describe('Compare Screenshots', function() {
       if (err) console.log(err);
       console.log("Successfully Written to File.");
     });
-    Object.keys(failImages).length.should
+    return Object.keys(failImages).length.should
       .lessThan(
         1,
         "Where "+Object.keys(failImages).length+" generated images not equal. <a href='"+result_file+"' >here</a>"
-    );
+      );
   });
 });
 
-async function checkDirectory(dir_name) {
+function checkDirectory(dir_name) {
   fs.existsSync(dir_name).should.equal(true,'Directory: '+dir_name+' not exist');
   fs.lstatSync(dir_name).isDirectory().should.equal(true,'Path: '+dir_name+' not directory');
   fs.readdirSync(dir_name).length.should.greaterThan(0,'Directory: '+dir_name+' is empty');
@@ -108,7 +153,7 @@ async function generateFailImagesResultPage(failImages){
       path.join(test_dir,'/views/test_results.ejs'),
       {
         live_screenshots_path:live_screenshots_path,
-        regenerated_screenshots_path: regenerated_screenshots_path,
+        regenerated_screenshots_path: test_screenshots_path,
         diff_screenshots_path:diff_screenshots_path,
         failImages: failImages,
       },
@@ -121,13 +166,11 @@ async function generateFailImagesResultPage(failImages){
   })
 }
 
-async function compareScreenshots(imageName, options) {
-  return await gmCompareImagesWrapper(live_screenshots_path+'/'+imageName,regenerated_screenshots_path+'/'+imageName,options)
-}
+
 
 function gmCompareImagesWrapper(path1, path2, options) {
   return new Promise((resolve, reject)=> {
-    imageMagick().compare(path1, path2, options, function (err, isEqual, equality, raw) {
+    im.compare(path1, path2, options, function (err, isEqual, equality, raw) {
       if (err) reject(err);
 // console.log('The images are equal: %s', isEqual);
 // console.log('Actual equality: %d', equality);
@@ -135,4 +178,25 @@ function gmCompareImagesWrapper(path1, path2, options) {
       return resolve(isEqual);
     });
   })
+}
+
+function start_harmony(cb) {
+  // var jsHarmonyTutorials = require('./../jsHarmonyTutorials.js');
+  var jsHarmonyTutorials = require('./../index.js');
+  var jsh = new jsHarmonyTutorials.Application();
+  jsh.Config.appbasepath = path.join(__dirname,'test-app');
+  jsh.Run();
+  jsh.Config.onServerReady.push(function () {
+    jsh.Modules.jsHarmonyTutorials.generateScreenshots({screenshot_folder:'/test/screenshots/'},function () {
+      jsh.Servers['default'].Close();
+      jsh.Servers = {};
+      jsh={};
+      cb();
+    });
+  });
+}
+
+async function compareScreenshots(imageName, options) {
+  var r = await gmCompareImagesWrapper(live_screenshots_path+'/'+imageName,test_screenshots_path+'/'+imageName,options)
+  return r;
 }
--- test/test-app/app.config.js
+++ test/test-app/app.config.js
@@ -2,15 +2,7 @@ exports = module.exports = function(jsh, config, dbconfig){
 
   //Server Settings
   config.server.http_port = 8090;
-  //config.server.https_port = 8081;
-  //config.server.https_cert = 'path/to/https-cert.pem';
-  //config.server.https_key = 'path/to/https-key.pem';
-  //config.server.https_ca = 'path/to/https-ca.crt';
   config.frontsalt = "CaXH-~#wLf,iHk1c9x*Y[We}qxpc&Cm&-tZj#tV{H{(=A?(cnMs>6vV7NtM0";
-    // console.log(path.dirname(require.main.filename));
-    // config.appbasepath = path.dirname(__filename);
-    // config.datadir = path.dirname(require.main.filename) + '/data/'; //Dynamic data folder (logs, temp files, user data)
-    // config.datadir = path.join(path.dirname('C:/Users/dkomsa/js/fork-js-tutorials/test/app.test.js1') , 'data'); //Dynamic data folder (logs, temp files, user data)
   //jsHarmony Factory Configuration
   var configFactory = config.modules['jsHarmonyFactory'];
   
@@ -18,13 +10,4 @@ exports = module.exports = function(jsh, config, dbconfig){
   configFactory.clientcookiesalt = "G>A}+o8{ah1c9rIKGP&yca^Ae*,7*Tbbc$n[33a>#i*s2oZ.Lq9zj3zM]Q&G";
   configFactory.mainsalt = "gR7?18.eMd3S3jVM6HI{.Xf$S,kN80nebM-5b{u4j$?Dwzngnk_d#p%cf<J{";
   configFactory.maincookiesalt = "ro_qes$M-K1K+NCIlwvjdFYw?(GU%c$D1[4}[I9Y0&xOZs}Zq,j8g>O~x(N#";
-  
-  config.onServerReady.push(function(cb, servers){
-    var port = jsh.Config.server.http_port;
-    if(jsh.Servers['default'] && jsh.Servers['default'].servers && jsh.Servers['default'].servers.length) port = jsh.Servers['default'].servers[0].address().port;
-    jsh.Modules.jsHarmonyTutorials.generateScreenshots({screenshot_folder:'/test/screenshots/'},function () {
-        process.exit(0);
-    });
-    return cb();
-  });
 }
